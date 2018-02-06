d16a06a2c34ec487ce7c2504afc6722546d97c63
Ignasi Llopis <iglloala@gmail.com>
2018-02-06 13:40:39 +0100

    Añadida función de geolocalización
    Se ha añadido la función de mostrar un mapa con la ubicación de la dirección del cliente en ClienteView haciendo uso de la api de mapas de google y la api geocoding para transformar direcciones trextuales en coordenadas geográficas.

--- js/ClienteView.js
+++ js/ClienteView.js
@@ -23,6 +23,10 @@ var ClienteView = (function(){
 		_ocultar();
 	});
 
+	// Eventos para disparar la api geocoding y generar un mapa para incluir en la ventana modal
+	$('#contenido').on('change', '#formClienteView #direccion', _cargarMapa);
+	//$('#contenido #formClienteView #direccion').on('load', _cargarMapa);
+
 	// MÉTODOS
 	function _init(){
 		// Registro un helper de handlebars para añadir el atributo cheked a un radiobutton
@@ -54,13 +58,18 @@ var ClienteView = (function(){
 		// En ambos casos convierto #inputFecha en un datepicker
 		_bloqueContenido.find('#inputFecha').datepicker({changeYear:'true', changeMonth:'true', yearRange:'1900:2018', dateFormat:'dd/mm/yy'});
 		_bloqueContenido.find('#fecha_alta').datepicker({changeYear:'true', changeMonth:'true', yearRange:'1900:2018', dateFormat:'dd/mm/yy'});
-		// Y llama a _mostrar para que se vea le modal
+		// Y llama a _mostrar para que se vea el modal
 		_mostrar();
 	}
 
 	// Función que muestra la ventana modal de ClienteView
 	function _mostrar(){
 		_bloqueContenido.find('#clienteView').modal("show");
+		// Añado un pequeño timeout xq si no a veces no carga bien el mapa
+		setTimeout(function(){
+			// Si existe una dirección en la ventana modal, llamo a _cargarMapa
+			if ($('#formClienteView #direccion').val() !== ""){_cargarMapa()}
+		}, 200);
 	}
 
 	// Función que oculta la ventana modal de ClienteView
@@ -89,6 +98,46 @@ var ClienteView = (function(){
 		}
 		// Y retorno un objeto plano con todos los datos
 		return datos;
+	}
+
+	function _cargarMapa(){
+		// Preparo los datos
+		var contenedor = $('#wrapper-mapa');
+		var calle = $('#formClienteView #direccion').val();
+		var ciudad = $('#formClienteView #ciudad').val();
+		var provincia = $('#formClienteView #provincia').val();
+		var address = calle + " " + ciudad + " " + provincia;
+		var key = "AIzaSyD8QjUGg_tSHL7Nxk6L-30jt6nOb_conGU";
+		var language = "es";
+		var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+encodeURI(address)+"&language="+language+"&key="+key;
+		// Inicio una petición ajax
+		var jqxhr = $.ajax({url:url});
+		jqxhr.done(function(respuesta){
+			// Si la respuesta es válida tiene que generar un mapa de google
+			if (respuesta.status == "OK"){
+				// Obtengo las coordenadas
+				var lat = respuesta.results[0].geometry.location.lat;
+				var lng = respuesta.results[0].geometry.location.lng;
+				var coords = {lat: lat, lng: lng};
+				// Preparo el contenedor
+				contenedor.css("margin", "5px 0");
+				contenedor.css("width", "466px");
+				contenedor.css("height", "233px");
+				// Genero el mapa
+				var mapa = new google.maps.Map(contenedor[0], 
+					{
+						zoom:17, 
+						center:coords, 
+						disableDefaultUI: true,
+					});
+				var marcador = new google.maps.Marker({position:coords, map:mapa});
+			}
+			// Si no que informe de que no se encuentra la dirección
+			else {
+				var mensaje = $('<p>').append("No se ha podido localizar la dirección");
+				contenedor.html(mensaje);
+			}
+		});
 	}
 
 	// EJECUCIÓN
